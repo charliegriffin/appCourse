75b59b0a20851f5ec84d9752ac6c1ae82cfe5e8f
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-03-08 06:41:31 -0600

    Add cloud database chat app

---
+++ Closures.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let array = [6,2,3,9,4,1]
+
+func addOne (n1: Int) -> Int {
+    return n1 + 1
+}
+
+let newArray = array.map {"\($0)"}
+print(newArray)
